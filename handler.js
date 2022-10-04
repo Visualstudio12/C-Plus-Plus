427dc6221b252a23f3179f2351eee2b1b8efa1dd
Mohit Kumar <mkay6123@gmail.com>
2022-10-04 15:19:25 +0530

    Create handler.js

---
+++ handler.js
@@ -0,0 +1,11 @@
+handlers.invites = {
+	index: function() {
+		var slide = false;
+		if(turtl.controllers.pages.is([NotesIndexController, SpacesSharingController])) {
+			slide = 'left';
+		}
+		turtl.controllers.pages.load(InvitesController, {}, {
+			slide: slide
+		});
+	},
+};
